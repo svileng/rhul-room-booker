0d18d284bf3712f1ec844d887dcaf4b7d0abb212
Svilen Gospodinov <svilen.gospodinov@gmail.com>
2012-04-06 00:21:47 +0100

    First commit

---
+++ RoomBooker/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace RoomBooker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtPostData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPostData
+            // 
+            this.txtPostData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPostData.Location = new System.Drawing.Point(12, 73);
+            this.txtPostData.Name = "txtPostData";
+            this.txtPostData.Size = new System.Drawing.Size(356, 20);
+            this.txtPostData.TabIndex = 0;
+            this.txtPostData.Text = "&duration=120&roomid=29&starttime=##########&preferredname=S.G.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "POST data";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(154, 110);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(103, 34);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(12, 25);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(169, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(197, 25);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(169, 20);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(194, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 163);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(354, 122);
+            this.txtLog.TabIndex = 9;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 124);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(126, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Start automatic booker at:";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(263, 110);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(103, 34);
+            this.btnStop.TabIndex = 12;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 297);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPostData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RHUL Automatic Room Booker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPostData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ RoomBooker/MainForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace RoomBooker
+{
+    public partial class MainForm : Form
+    {
+        #region Private Instance Variables
+        /// <summary>Shared cookie container between login/booking requests</summary>
+        private CookieContainer cookieContainer = new CookieContainer();
+        /// <summary>Time to start thread</summary>
+        private DateTime startingTime;
+        /// <summary>Placeholder for error messages from the background thread</summary>
+        private string message = string.Empty;
+
+        #endregion
+
+        #region Constructors
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Private Methods
+
+        private void BookRoom(string loginParams, string bookingParams)
+        {
+            string loginResponse = SendLoginRequest(loginParams);
+
+            if (loginResponse.Contains("<authenticated>true</authenticated>"))
+            {
+                string bookingResponse = SendBookingRequest(bookingParams);
+                if (bookingResponse.Contains("<strong>Error</strong><br/>You may not make reservations more than 2 weeks in advance.<br/>"))
+                {
+                    throw new Exception("You may not make reservations more than 2 weeks in advance.");
+                }
+                else if (bookingResponse.Contains("<strong>Error</strong><br/>Another room has been reserved during this time.<br/>You may only make 120 minutes worth of reservations per day.<br/>"))
+                {
+                    throw new Exception("Another room has been reserved / you already booked 120 mins for that day.");
+                }
+                else if (bookingResponse.Contains("<strong>Error</strong><br/>Another room has been reserved during this time.<br/>"))
+                {
+                    throw new Exception("Room is already reserved for this time.");
+                }
+            }
+            else
+            {
+                throw new Exception("Authentication failed.");
+            }
+        }
+
+        private string SendBookingRequest(string bookingParams)
+        {
+            HttpWebRequest bookRequest = (HttpWebRequest)HttpWebRequest.Create("https://libraryrooms.rhul.ac.uk/or-reserve.php");
+            bookRequest.Method = "POST";
+            bookRequest.ContentType = "application/x-www-form-urlencoded";
+            bookRequest.CookieContainer = cookieContainer;
+
+            byte[] requestData = Encoding.UTF8.GetBytes(bookingParams);
+            using (Stream dataStream = bookRequest.GetRequestStream())
+            {
+                dataStream.Write(requestData, 0, requestData.Length);
+            }
+
+            HttpWebResponse bookingResponse = (HttpWebResponse)bookRequest.GetResponse();
+            Stream bookingResponseStream = bookingResponse.GetResponseStream();
+            StreamReader reader = new StreamReader(bookingResponseStream);
+            string responseString = reader.ReadToEnd();
+
+            return responseString;
+        }
+
+        private string SendLoginRequest(string loginParams)
+        {
+            try
+            {
+                HttpWebRequest loginRequest = (HttpWebRequest)HttpWebRequest.Create("https://libraryrooms.rhul.ac.uk/or-authenticate.php");
+
+                loginRequest.Method = "POST";
+                loginRequest.ContentType = "application/x-www-form-urlencoded";
+                loginRequest.CookieContainer = cookieContainer;
+
+                byte[] loginRequestData = Encoding.UTF8.GetBytes(loginParams);
+                using (Stream dataStream = loginRequest.GetRequestStream())
+                {
+                    dataStream.Write(loginRequestData, 0, loginRequestData.Length);
+                }
+
+                HttpWebResponse loginResponse = (HttpWebResponse)loginRequest.GetResponse();
+                Stream loginResponseStream = loginResponse.GetResponseStream();
+                StreamReader reader = new StreamReader(loginResponseStream);
+                string responseString = reader.ReadToEnd();
+
+                return responseString;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Login request failed (connection problem)");
+            }
+        }
+
+        private void RunBackgroundWorker()
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string postData = txtPostData.Text;
+
+            string loginParams = "username=" + username + "&password=" + password + "&ajax_indicator=TRUE";
+            string bookingParams = "altusername=&emailconfirmation=&capacity=4&fullcapacity=8" + postData;
+
+            backgroundWorker.RunWorkerAsync(new string[] { loginParams, bookingParams });
+        }
+
+        private void OutputMessage(string msg)
+        {
+            txtLog.Text += DateTime.Now.ToLongTimeString() + " - " + msg + "\r\n";
+            txtLog.SelectionStart = txtLog.TextLength;
+            txtLog.ScrollToCaret();
+        }
+
+        #endregion
+
+        #region Events
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                OutputMessage("Starting timer...");
+                startingTime = DateTime.Parse(dateTimePicker1.Text);
+                timer.Enabled = true;
+                timer.Start();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Compare(DateTime.Now, startingTime) >= 0)
+            {
+                timer.Stop();
+                OutputMessage("Timer stopped; running automatic booker.");
+                RunBackgroundWorker();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.CancelAsync();
+            timer.Stop();
+            OutputMessage("System stopped by user.");
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] args = (string[])e.Argument;
+            try
+            {
+                BookRoom(args[0], args[1]); // login request data and booking request data params
+            }
+            catch (Exception ex)
+            {
+                e.Cancel = true;
+                message = ex.Message;
+            }
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null || e.Cancelled)
+            {
+                Exception ex = e.Error;
+                OutputMessage("Error: " + message);
+
+                if (message.Contains("weeks in advance") || message.Contains("request failed"))
+                {
+                    // brute-force booking until it succeeds
+                    // or room is taken by someone else (tiny possibility for the latter)
+                    RunBackgroundWorker();
+                }
+                else
+                {
+                    OutputMessage("Stopping automatic booker...");
+                }
+            }
+            else
+            {
+                OutputMessage("Room booked successfully!");
+            }
+        }
+
+        #endregion
+    }
+}
